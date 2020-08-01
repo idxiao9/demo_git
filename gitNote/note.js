08cdd0eeea2e3c6cab50ee0ad32175fa14cdfbb6
carel <3002840525@qq.com>
2020-08-01 17:59:10 +0800

    update at 2020 /8 /1

--- gitNote/note.js
+++ gitNote/note.js
@@ -1,7 +1,14 @@
 // here I keep my note about git
-git comands:
-git add
-git commit -m "some describes"
-git rm filename
-git checkout
-git status
+// git comands:
+// git add filename 提交工作区已经修改的文件到暂存区
+// git commit -m "some describes" 将暂存区的文件或修改记录更新到git仓库，同时创建log id 用来标注回滚位置
+// git rm filename 和 rm filename 前者直接提交到暂存区，后者只是在工作区删除此文件
+// git checkout 清空未add到暂存区的工作区内容
+// git status 查看 git 仓库状态
+// git init 初始化git仓库
+// echo " # demo_git" >> filename 从行程仓库获取文件
+// git remote add origin git@github.com:idxiao9/demo_git.git 创建远程连接
+// git push -u origin master 提交本地git仓库内容到远程仓库 以后可以只用git push
+// ll 查看现在目录下的文件 ls -a 查看目录结构
+// cat filename 显示文件内容 vim filename 编辑文件，如果文件不存在，则会先新建。
+// vim 编辑时，i 写入内容，esc 退出写入 ：wq 退出编辑
