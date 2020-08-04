b968578a29f8d10b2480b529f002d5af82bc7a52
carel <3002840525@qq.com>
2020-08-04 17:49:53 +0800

    added new info at 8.4

--- gitNote/note.js
+++ gitNote/note.js
@@ -1,19 +1,24 @@
 // here I keep my note about git 
 // git有图形化界面方便使用，sourcetree
 // git comands:
+// git config --global user.name "someone" 首次安装git后要配置用户
+// git config --global user.email "emailaddress"
+// git config -list 查看配置信息
+
 // git add filename 提交工作区已经修改的文件到暂存区
 // git commit -m "some describes" 将暂存区的文件或修改记录更新到git仓库，同时创建log id 用来标注回滚位置
 // git rm filename 和 rm filename 前者直接提交到暂存区，后者只是在工作区删除此文件
 // git checkout 清空未add到暂存区的工作区内容
 // git status 查看 git 仓库状态
 // git init 初始化git仓库
-// echo " # demo_git" >> filename 从行程仓库获取文件
+// echo " # demo_git" >> filename 从远程仓库获取文件 这个理解是错误的
 // git remote add origin git@github.com:idxiao9/demo_git.git 创建远程连接
 // git push -u origin master 提交本地git仓库内容到远程仓库 第一次
 // 提交需要-u origin master 指向一下分支。 以后可以只用git push
 // ll 查看现在目录下的文件 ls -a 列出文件夹下所有文件，包括隐藏文件
 // cat filename 显示文件内容 vim filename 编辑文件，如果文件不存在，则会先新建。
 // vim 编辑时，i 写入内容，esc 退出写入 ：wq 退出编辑
+// git pull 从远程仓库获取文件
 // git clone git@github.com:idxiao9/demo_git 克隆远程仓库，用来克隆的文件夹必须没有
 // 被 git init 过。
 // pwd 查看当前文件位置，git log 查看commit记录 git reset --hard id 回滚
@@ -21,9 +26,9 @@
 
 
 // 标签管理
-// git tag 查看所有标签 git tag filename 创建标签
-// git tag -a filename -m "some info" 指定提交时的信息 git tag -d filename 删除标签
-// git push origin filename 标签发布
+// git tag 查看所有标签 git tag tagName 创建标签
+// git tag -a tagName -m "some info" 指定提交时的信息 git tag -d tagName 删除标签
+// git push origin tagName 标签发布
 
 // 分支管理
 // git branch branchName 创建分支  git branch 查看所有分支
---
+++ linuxNote/linux.js
@@ -0,0 +1,13 @@
+Linux 笔记
+Linux 和 windows的区别
+Linux 严格区分大小写，Linux中所有内容都以文件形式保存，包括硬件
+Linux 中没有扩展名，但是有约定的文件类型，用于方便管理员区分文件类型：
+压缩包 ： *.gz ,*.bz2 *.tra.bz2 *.tgz 等
+二进制软件包： .rpm
+网页文件 ： *.html *.php
+脚本文件： *.sh
+配置文件： *.conf
+windows下的程序不能直接在Linux中安装和运行
+
+
+虚拟机 VMware 官网http://www.vmware.com
